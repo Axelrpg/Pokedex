550e5467ebb33e85bef77a8083434325b96f77b2
Axel García <111660292+Axelrpg@users.noreply.github.com>
2024-08-06 17:56:28 -0600

    feat: Add dark and light color themes

---
+++ src/colors/DarkColors.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const darkColors = StyleSheet.create({
+    primaryBackground: {
+        color: '#121212',
+    },
+    primaryText: {
+        color: '#FFFFFF',
+    },
+    rippleColor: {
+        color: 'rgba(255, 255, 255, .32)',
+    },
+});
---
+++ src/colors/LightColors.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const lightColors = StyleSheet.create({
+    primaryBackground: {
+        color: '#FFFFFF', // Fondo claro
+    },
+    primaryText: {
+        color: '#121212', // Texto oscuro para contraste
+    },
+    rippleColor: {
+        color: 'rgba(0, 0, 0, .32)', // Color del efecto ripple en modo claro
+    },
+});
